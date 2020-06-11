8c7a2e66402d97aa26e680ca973f3ba6ccdd6074
= <alex.a4.25@yandex.ru>
2020-06-11 16:05:14 +0300

    Init client

---
+++ src/http_client/LoansClient.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+// Корневой урл для обращения к серверу
+let url = 'https://loans-analyzer.herokuapp.com';
+
+// Гет запрос к серверу с путём
+// @param {string} path - Путь запроса к серверу, указывается с / в начале
+// @return {Promise} - асинхронный запрос к сети
+export function get(path: string): Promise {
+    return axios.get(url + path);
+}
+
+// Пост запрос к серверу с путём
+// @param {string} path - Путь запроса к серверу, указывается с / в начале
+// @param {any} data - данные для запроса
+// @return {Promise} - асинхронный запрос к сети
+export function post(path: string, data?: any): Promise {
+    return axios.post(url + path, data);
+}
+
+// Пут запрос к серверу с путём
+// @param {string} path - Путь запроса к серверу, указывается с / в начале
+// @param {any} data - данные для запроса
+// @return {Promise} - асинхронный запрос к сети
+export function put(path: string, data?: any): Promise {
+    return axios.put(url + path, data);
+}

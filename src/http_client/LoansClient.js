631ff9f2b9011902236fc150db7c5b7ac79245b4
= <alex.a4.25@yandex.ru>
2020-06-12 17:27:52 +0300

    Fix client

--- src/http_client/LoansClient.js
+++ src/http_client/LoansClient.js
@@ -1,4 +1,4 @@
-import axios, {AxiosRequestConfig} from 'axios';
+import axios from 'axios';
 
 // Корневой урл для обращения к серверу
 let url = 'https://loans-analyzer.herokuapp.com';
@@ -6,15 +6,15 @@ let url = 'https://loans-analyzer.herokuapp.com';
 // Гет запрос к серверу с путём
 // @param {string} path - Путь запроса к серверу, указывается с / в начале
 // @return {Promise} - асинхронный запрос к сети
-export function get(path: string, config: AxiosRequestConfig): Promise {
-    return axios.get(url + path);
+export function get(path: string, data?: any): Promise {
+    return axios.get(url + path, data);
 }
 
 // Пост запрос к серверу с путём
 // @param {string} path - Путь запроса к серверу, указывается с / в начале
 // @param {any} data - данные для запроса
 // @return {Promise} - асинхронный запрос к сети
-export function post(path: string, data?: any, config: AxiosRequestConfig): Promise {
+export function post(path: string, data?: any): Promise {
     return axios.post(url + path, data);
 }
 
@@ -22,6 +22,6 @@ export function post(path: string, data?: any, config: AxiosRequestConfig): Prom
 // @param {string} path - Путь запроса к серверу, указывается с / в начале
 // @param {any} data - данные для запроса
 // @return {Promise} - асинхронный запрос к сети
-export function put(path: string, data?: any, config: AxiosRequestConfig): Promise {
+export function put(path: string, data?: any): Promise {
     return axios.put(url + path, data);
 }

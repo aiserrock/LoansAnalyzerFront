790a85e45e8dcd5ab1be7829792a52fb4e7acb76
= <alex.a4.25@yandex.ru>
2020-06-12 19:23:29 +0300

    Complete loans controller

--- src/controllers/LoansController.js
+++ src/controllers/LoansController.js
@@ -1,61 +1,94 @@
 import {get, post, put} from "../http_client/LoansClient";
 
 // Контроллер, который обрабатывает запросы замймов
+// !!! Если была получена ошибка, то методы будут возвращать её код
+// 401 - ошибка авторизации, 404 - отсутствует, 422 - ошибка парсинга
 export class LoansController {
     // Получение списка займов.
     // @param token - токен авторизации пользователя
     // @param skip - сколько записей пропустить, реализует пагинацию
-    async getLoans(token: string, skip = 0) {
-        let response = await get(`/loans/?skip=${skip}`, {
-            headers: {
-                "Authorization": `Bearer ${token}`,
-            },
-        });
+    // @param search - строка поиска, если производится поиск, параметр опционален
+    // @return Возвращает список найденных займов
+    // Чтобы сделать отступ, указываем первые два параметра
+    // Чтобы сделать поиск, указываем три параметра, при этом skip=null
+    async getLoans(token: string, skip = 0, search: string) {
+        try {
+            let param = '';
+            if (skip !== 0 && skip !== null) {
+                param = `?skip=${skip}`;
+            }
+            if (search !== undefined && search !== null) {
+                param = `?search=${search}`;
+            }
+            let response = await get(`/loans/${param}`,
+                {
+                    headers: {
+                        "Authorization": `Bearer ${token}`,
+                    },
+                });
 
-        return response.data;
+            return response.data;
+        } catch (e) {
+            return e.response.status;
+        }
     }
 
     // Создание займа
     // @param token - токен авторизации пользователя
     // @param data - данные займа
+    // @return Возвращает созданный объект
     async createLoan(token: string, data) {
-        let response = await post('/loans/', {
-            headers: {
-                "Content-Type": 'application/json',
-                "Authorization": `Bearer ${token}`,
-            },
-            data: data,
-        });
+        try {
+            let response = await post('/loans/', data, {
+                headers: {
+                    "Content-Type": 'application/json',
+                    "Authorization":
+                        `Bearer ${token}`
+                    ,
+                },
+            });
 
-        return response.data;
+            return response.data;
+        } catch (e) {
+            return e.response.status;
+        }
     }
 
     // Получение одного займа по id
     // @param token - токен авторизации пользователя
     // @param id - идентификатор займа
+    // @return один объект займа
     async getLoanById(token: string, id) {
-        let response = await get(`/loans/${id}`, {
-            headers: {
-                "Authorization": `Bearer ${token}`,
-            }
-        });
+        try {
+            let response = await get(
+                `/loans/${id}`, {
+                    headers: {"Authorization": `Bearer ${token}`},
+                });
 
-        return response.data;
+            return response.data;
+        } catch (e) {
+            return e.response.status;
+        }
     }
 
     // Обновление займа по id
     // @param token - токен авторизации пользователя
     // @param id - идентификатор займа
     // @param data - данные займа
+    // @return Возвращает обновленный объект
     async updateLoanById(token: string, id, data) {
-        let response = await put(`/loans/${id}`, {
-            headers: {
-                "Authorization": `Bearer ${token}`,
-                "Content-Type": 'application/json',
-            },
-            data: data,
-        });
+        try {
+            let response = await put(
+                `/loans/${id}`, data, {
+                    headers: {
+                        "Authorization": `Bearer ${token}`,
+                        "Content-Type": 'application/json',
+                    },
+                });
 
-        return response.data;
+            return response.data;
+        } catch (e) {
+            return e.response.status;
+        }
     }
 }

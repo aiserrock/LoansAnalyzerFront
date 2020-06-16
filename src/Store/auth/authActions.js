83ff900dbb15e53201ad9dee398a9e26dde3324e
Никита <nikita-dmitr@yandex.ru>
2020-06-16 10:30:19 +0300

    Corrected

--- src/Store/auth/authActions.js
+++ src/Store/auth/authActions.js
@@ -6,15 +6,16 @@ import {AUTH_ERROR, AUTH_SUCCESS} from './actionTypes'
 export function auth(login, password) {
     return async (dispatch) => {
         try {
-
             const data = await AuthController.prototype.auth((login, password))
 
-            console.log(login, password,data)
-
-            localStorage.setItem('data', JSON.stringify(data))
-            dispatch(dispatchAction(AUTH_SUCCESS, data))
-        }
-        catch (e) {
+            console.log(login, password, data)
+            if (data !== null) {
+                localStorage.setItem('data', JSON.stringify(data))
+                dispatch(dispatchAction(AUTH_SUCCESS, data))
+            } else{
+                dispatch(dispatchAction(AUTH_ERROR, null))
+            }
+        } catch (e) {
             dispatch(dispatchAction(AUTH_ERROR, null))
         }
     }
